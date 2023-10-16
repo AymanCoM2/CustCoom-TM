b9e36d9bf78ec6cf5dcc8c5ecef55053babd2a79
= <aymancoom3@gmail.com>
2023-10-16 15:52:25 +0300

    TM

--- app/Console/Commands/starting.php
+++ app/Console/Commands/starting.php
@@ -16,6 +16,7 @@ class starting extends Command
 
     public function handle()
     {
+        User::truncate();
         $adminUser = new User();
         $adminUser->name = 'admin';
         $adminUser->email = 'admin@admin.com';
@@ -27,18 +28,39 @@ class starting extends Command
 
         $editorUser = new User();
         $editorUser->name = 'editor';
-        $editorUser->email = 'editor@editor.com';
+        $editorUser->email = 'aymancoom3@gmail.com';
         $editorUser->password = Hash::make('123');
         $editorUser->isSuperUser = 2;
         $editorUser->email_verified_at = now();
         $editorUser->save();
 
+
+
+        $editorUser2 = new User();
+        $editorUser2->name = 'editor';
+        $editorUser2->email = 'editor2@editor.com';
+        $editorUser2->password = Hash::make('123');
+        $editorUser2->isSuperUser = 2;
+        $editorUser2->email_verified_at = now();
+        $editorUser2->save();
+
+
+
         $viewerUser = new User();
         $viewerUser->name = 'viewer';
-        $viewerUser->email = 'viewer@viewer.com';
+        $viewerUser->email = 'viewer1@viewer.com';
         $viewerUser->password = Hash::make('123');
         $viewerUser->isSuperUser = 3;
         $viewerUser->email_verified_at = now();
         $viewerUser->save();
+
+
+        $viewerUser2 = new User();
+        $viewerUser2->name = 'viewer';
+        $viewerUser2->email = 'viewer2@viewer.com';
+        $viewerUser2->password = Hash::make('123');
+        $viewerUser2->isSuperUser = 3;
+        $viewerUser2->email_verified_at = now();
+        $viewerUser2->save();
     }
 }
