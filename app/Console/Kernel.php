77e3fbc67cfef5ea116b434f295746b02d3fc6b9
Ayman <ayman.salah@2coom.com>
2023-12-17 09:36:22 +0200

    At 11

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:do-backup')->saturdays();
+        $schedule->command('app:do-backup')->saturdays()->at('11:00');
     }
 
     /**
