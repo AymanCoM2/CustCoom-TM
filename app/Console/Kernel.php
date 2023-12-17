82368edc22799420cca2f301c6c1f4165a4be0e7
Ayman <ayman.salah@2coom.com>
2023-12-17 09:46:56 +0200

    Done For Subnday

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:do-backup')->mondays()->at('11:00');
+        $schedule->command('app:do-backup')->sundays()->at('11:00');
     }
 
     /**
